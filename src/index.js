fb460e5a37aba41c2726480cd3224e96199d8ff4
badman007 <1572226525@qq.com>
2022-07-11 21:51:20 +0800

    'init'

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from '@reduxjs/toolkit';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const ADD = 'ADD';
+const MINUS = 'MINUS';
+
+function add() {
+  return {type: ADD}
+}
+
+function minus() {
+  return {type: MINUS};
+}
+
+function reducer(state={number: 0}, action) {
+  switch(action.type) {
+    case ADD: 
+      return {number: state.number + 1};
+    case MINUS: 
+      return {number: state.number - 1};
+    default: 
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+const valueEle = document.getElementById('value');
+const addEle = document.getElementById('add');
+const minusEle = document.getElementById('minus');
+
+// subscribe  dispatch getState
+
+function render() {
+  valueEle.innerHTML = store.getState().number;
+}
+
+render();
+
+store.subscribe(render);
+
+addEle.addEventListener('click', () => {
+  store.dispatch(add());
+})
+
+minusEle.addEventListener('click', () => {
+  store.dispatch(minus());
+})
